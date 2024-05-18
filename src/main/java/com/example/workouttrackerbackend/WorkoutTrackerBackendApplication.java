39236c5d1617c15a6002ba5cec89bee6cc2c117c
Kr-Stam <kristijanstameski@gmail.com>
2024-05-18 12:23:58 +0200

    Initial commit

---
+++ src/main/java/com/example/workouttrackerbackend/WorkoutTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.workouttrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutTrackerBackendApplication.class, args);
+    }
+
+}
